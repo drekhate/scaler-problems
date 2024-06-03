91a8c44fd00573622c9a11f530d7a2d0a05f8459
direkhate <direkhate@gmail.com>
2024-06-03 10:34:44 +0530

    MostFrequentCharInString problem

---
+++ src/scaler/MostFrequentCharInString.java
@@ -0,0 +1,28 @@
+package scaler;
+
+import java.util.Scanner;
+
+public class MostFrequentCharInString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char ans = mostFrequentCharInString(str);
+        System.out.println(ans);
+    }
+    public static char mostFrequentCharInString(String str) {
+        int[] freq = new int[128];
+        int count = 0;
+        char ans = '\u0000';
+        for (int i = 0; i < str.length(); i ++) {
+            char ch = str.charAt(i);
+            freq[ch] ++;
+        }
+        for (int i = 0; i < freq.length; i ++) {
+            if(freq[i] > count) {
+                count = freq[i];
+                ans = (char) i;
+            }
+        }
+        return ans;
+    }
+}
