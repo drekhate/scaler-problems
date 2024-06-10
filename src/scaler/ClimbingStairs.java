158918541fb3c95438467b6da98106793ef81440
direkhate <direkhate@gmail.com>
2024-06-10 10:51:38 +0530

    climbing stairs problem done

---
+++ src/scaler/ClimbingStairs.java
@@ -0,0 +1,22 @@
+package scaler;
+
+import java.util.Scanner;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(climbingStairs(n));
+    }
+
+    private static int climbingStairs(int n) {
+        int ans = 0;
+        int[] arr = new int[n + 1];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i ++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
